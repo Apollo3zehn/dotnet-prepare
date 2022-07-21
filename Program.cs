5e34bd398befd3cc156d1d0128b2369dbcd45a5a
Apollo3zehn <git@m1.apollo3zehn.net>
2022-07-21 16:53:39 +0200

    Prepare tool.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,81 @@
-﻿Console.WriteLine($"Hi {Environment.UserName}!");
+﻿using System.Reflection;
+
+Console.WriteLine("Welcome to Apollo3zehn's project initializer.");
+Walk();
+
+static void Walk(string root = ".")
+{
+    if (IsGitRepository(root))
+    {
+        Console.WriteLine($"Directory {root} is a GIT repository. Prepare repository now.");
+
+        var originalDir = Environment.CurrentDirectory;
+
+        try
+        {
+            Environment.CurrentDirectory = root;
+            Prepare();
+        }
+        finally
+        {
+            Environment.CurrentDirectory = originalDir;
+        }
+    }
+
+    else
+    {
+        Console.WriteLine($"Directory {root} is not a GIT repository. Iterate through sub repositories now.");
+        var any = false;
+
+        foreach (var directory in Directory.EnumerateDirectories("."))
+        {
+            any = true;
+            Walk(directory);
+        }
+
+        if (!any && root == "")
+            Console.WriteLine($"No GIT repositories found. Bye Bye.");
+    }
+}
+
+static bool IsGitRepository(string root)
+{
+    return Directory.Exists(Path.Combine(root, ".git"));
+}
+
+static void Prepare()
+{
+    const string SOLUTION = "solution.json";
+    const string VERSION = "version.json";
+
+    var resourceFolderPath = Path.Combine(Assembly.GetExecutingAssembly().Location, "..", "resources");
+
+    if (!File.Exists(SOLUTION))
+    {
+        Console.WriteLine($"Create file solution.json");
+        File.Copy(Path.Combine(resourceFolderPath, SOLUTION), SOLUTION);
+    }
+
+    if (!File.Exists(VERSION))
+    {
+        Console.WriteLine($"Create file version.json");
+        File.Copy(Path.Combine(resourceFolderPath, VERSION), VERSION);
+    }
+
+    foreach (var filePath in Directory.EnumerateFiles(resourceFolderPath, "*.*", SearchOption.AllDirectories))
+    {
+        if (filePath.EndsWith(SOLUTION) || filePath.EndsWith(VERSION))
+            continue;
+
+        var targetFilePath = filePath[(resourceFolderPath.Length + 1)..];
+        var targetFolderPath = Path.GetDirectoryName(targetFilePath);
+
+        if (!string.IsNullOrWhiteSpace(targetFolderPath))
+            Directory.CreateDirectory(targetFolderPath);
+
+        Console.WriteLine($"Create file {filePath[(resourceFolderPath.Length + 1)..]}");
+        File.Copy(filePath, targetFilePath, overwrite: true);
+    }
+
+    Console.WriteLine($"Done.");
+}
---
+++ resources/build/print_solution.py
@@ -0,0 +1,15 @@
+import json
+
+with open("solution.json", "r") as fh:
+    solution_data = json.load(fh)
+        
+print(
+f"""
+AUTHORS={solution_data["authors"]}
+COMPANY={solution_data["company"]}
+COPYRIGHT={solution_data["copyright"]}
+PRODUCT={solution_data["product"]}
+LICENSE={solution_data["license"]}
+PROJECT_URL={solution_data["project-url"]}
+REPOSITORY_URL={solution_data["repository-url"]}
+""")
