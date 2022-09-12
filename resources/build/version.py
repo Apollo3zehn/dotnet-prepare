dc78d7d6d92a573f56c0f7c1437fed8f000d36a3
Apollo3zehn <git@m1.apollo3zehn.net>
2022-09-12 12:01:43 +0200

    Add option to append git commit hash.

--- resources/build/version.py
+++ resources/build/version.py
@@ -1,12 +1,16 @@
 import json
+import subprocess
 from typing import Optional
 
 
-def get_version(build: Optional[str], as_pypi_version: bool = False) -> str:
+def get_version(build: Optional[str], version_type: str = "default") -> str:
 
     with open("version.json", "r") as fh:
         version_data = json.load(fh)
-        
+
+    # git commit hash
+    commit = subprocess.check_output(["git", "rev-parse", "HEAD"], stdin=None, stderr=None, shell=False).decode("utf8").strip()
+
     # version
     version = version_data["version"]
     suffix = version_data["suffix"]
@@ -17,10 +21,13 @@ def get_version(build: Optional[str], as_pypi_version: bool = False) -> str:
         if build:
 
             # PEP440 does not support SemVer versioning (https://semver.org/#spec-item-9)
-            if as_pypi_version:
+            if version_type == "pypi":
                 version = f"{version}{int(build):03d}"
 
             else:
                 version = f"{version}.{build}"
+
+    if (version_type == "assembly"):
+        version += "+" + commit
 
     return version
